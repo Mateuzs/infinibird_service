19d2e9e7777bdd4b1ef3640f303c457a15c47901
Mateusz Zembol <Mat@macbook-pro-mat.home>
2019-06-23 15:38:42 +0200

    preparing configs for deployment

---
+++ config/prod.exs
@@ -0,0 +1,13 @@
+config :infinibird_service, InfinibirdService.Endpoint,
+  # Possibly not needed, but doesn't hurt
+  http: [port: {:system, "PORT"}],
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 80],
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  server: true
+
+config :infinibird_service, InfinibirdService.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  ssl: true,
+  # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections.
+  pool_size: 2
