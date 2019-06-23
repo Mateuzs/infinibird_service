fb30db0d23c6ef3b3a3085a6092a545004ac28df
Mateuzs <mateusz.zembol@gmail.com>
2019-06-23 22:56:28 +0200

    https

--- config/prod.exs
+++ config/prod.exs
@@ -7,7 +7,6 @@ config :infinibird_service, InfinibirdService.Endpoint,
   secret_key_base: "${SECRET_KEY_BASE}",
   url: [host: "${APP_NAME}.gigalixirapp.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
-  force_ssl: [hsts: true],
 
 config :infinibird_service, InfinibirdService.Repo,
   adapter: Ecto.Adapters.Postgres,
@@ -16,3 +15,7 @@ config :infinibird_service, InfinibirdService.Repo,
   # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections.
   pool_size: 2
 
+# Do not print debug messages in production
+config :logger, level: :info
+
+config :infinibird, InfinibirdWeb.Endpoint, force_ssl: [rewrite_on: [:x_forwarded_proto]]
