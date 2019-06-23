77c08ced366fa42c255b300ce05f15426d015c17
Mateuzs <mateusz.zembol@gmail.com>
2019-06-23 22:53:57 +0200

    https

--- config/prod.exs
+++ config/prod.exs
@@ -7,7 +7,7 @@ config :infinibird_service, InfinibirdService.Endpoint,
   secret_key_base: "${SECRET_KEY_BASE}",
   url: [host: "${APP_NAME}.gigalixirapp.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
-  force_ssl: [rewrite_on: [:x_forwarded_proto]]
+  force_ssl: [hsts: true],
 
 config :infinibird_service, InfinibirdService.Repo,
   adapter: Ecto.Adapters.Postgres,
