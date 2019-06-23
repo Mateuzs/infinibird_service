bc9c85a5cf25af74b940668b58379957a1a282ba
Mateusz Zembol <Mat@MacBook-Pro-Mat.local>
2019-06-23 22:48:58 +0200

    https troubles

--- config/prod.exs
+++ config/prod.exs
@@ -1,12 +1,13 @@
 config :infinibird_service, InfinibirdService.Endpoint,
-  # Possibly not needed, but doesn't hurt
+  load_from_system_env: true,
+  # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
   http: [port: {:system, "PORT"}],
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 80],
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
-  server: true
+  # Without this line, your app will not start the web server!
+  server: true,
   secret_key_base: "${SECRET_KEY_BASE}",
   url: [host: "${APP_NAME}.gigalixirapp.com", port: 443],
-  cache_static_manifest: "priv/static/cache_manifest.json",
+  cache_static_manifest: "priv/static/cache_manifest.json"
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 config :infinibird_service, InfinibirdService.Repo,
   adapter: Ecto.Adapters.Postgres,
@@ -15,6 +16,3 @@ config :infinibird_service, InfinibirdService.Repo,
   # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections.
   pool_size: 2
 
-
-  ## forces to use https during communication
-  config :infinibird_service, InfinibirdService.Endpoint, force_ssl: [rewrite_on: [:x_forwarded_proto]]
