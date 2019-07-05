434e903942b6bf7691c7aaf7bb03be541dde07e1
Mateuzs <mateusz.zembol@gmail.com>
2019-07-05 09:09:25 +0200

    starting semantic versioning

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule InfinibirdService.MixProject do
   def project do
     [
       app: :infinibird_service,
-      version: "0.3.7",
+      version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps()
