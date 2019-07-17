c308aab935e27f4d3abacf99799068967ba2aa4a
Mateuzs <mateusz.zembol@gmail.com>
2019-07-17 17:27:08 +0200

    added improvements

--- lib/infinibird_service/controllers/tango/tango_controller.ex
+++ lib/infinibird_service/controllers/tango/tango_controller.ex
@@ -1,25 +1,30 @@
 defmodule InfinibirdService.TangoController do
   def handle_new_trip(device_id) do
-    IO.puts(device_id)
+    generate_token(device_id)
+  end
 
-    token = String.slice(device_id, (String.length(device_id) - 8)..String.length(device_id))
+  defp generate_token(device_id) do
+    normalized_device_id = String.replace(device_id, "-", "")
+
+    token =
+      String.slice(
+        normalized_device_id,
+        (String.length(normalized_device_id) - 8)..String.length(normalized_device_id)
+      )
 
     unique_token =
       case check_token_existence(token) do
         nil -> token
-        _else -> get_random_token(device_id)
+        _else -> get_random_token(normalized_device_id)
       end
 
     unique_token
   end
 
   defp get_random_token(device_id) do
-    normalized_device_id = String.replace(device_id, "-", "")
-    IO.puts(normalized_device_id)
-
     random_token =
-      Enum.take_random(0..(String.length(normalized_device_id) - 1), 8)
-      |> Enum.map(fn e -> String.at(normalized_device_id, e) end)
+      Enum.take_random(0..(String.length(device_id) - 1), 8)
+      |> Enum.map(fn e -> String.at(device_id, e) end)
 
     case check_token_existence(random_token) do
       nil -> random_token
