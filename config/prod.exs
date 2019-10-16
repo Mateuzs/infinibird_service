06bd725b00bc9c897f3087a2a509b015aa43f4d1
Mateuzs <mateusz.zembol@gmail.com>
2019-10-16 18:55:54 +0200

    fixing db config

--- config/prod.exs
+++ config/prod.exs
@@ -7,7 +7,8 @@ config :infinibird_service, InfinibirdService.Endpoint,
   url: [scheme: "https", host: "infinibird-service.gigalixirapp.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
-config :infinibird_service, InfinibirdService.Repo,
+# DB config
+config :infinibird_service, InfinibirdDB.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   ssl: true,
@@ -19,11 +20,3 @@ config :logger, level: :info
 
 config :infinibird_service, InfinibirdService.Endpoint,
   force_ssl: [rewrite_on: [:x_forwarded_proto], hsts: true, host: nil]
-
-# DB config
-config :infinibird_service, InfinibirdDB.Repo,
-  username: "b0ca5494-639d-4076-9597-fadfdffe61d8-user",
-  password: "pw-75c51fb8-61f6-4815-af31-57f841ccb809@postgres-free-tier-1",
-  database: "b0ca5494-639d-4076-9597-fadfdffe61d8",
-  hostname: "gigalixir.com",
-  pool_size: 10
